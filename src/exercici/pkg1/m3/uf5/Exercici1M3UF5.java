9aed27ee066c5c8412d599b8f1228226273acb18
davidblancolopez <daviddbl94@gmail.com>
2017-01-31 15:40:59 +0100

    CAMBIOS

--- src/exercici/pkg1/m3/uf5/Exercici1M3UF5.java
+++ src/exercici/pkg1/m3/uf5/Exercici1M3UF5.java
@@ -9,7 +9,7 @@ import java.util.logging.Logger;
 public class Exercici1M3UF5 {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws CloneNotSupportedException, InstantiationException, IllegalAccessException {
         Element e = new Element();
         InfoElement info = new InfoElement(e);
         
@@ -21,29 +21,19 @@ public class Exercici1M3UF5 {
         System.out.println("5. array de mètodes (Method): " + info.arrayMetodes());
         System.out.println("6. array dels noms dels atributs: " + info.arrayNomsAtributs());
         System.out.println("7. array dels noms dels mètodes: " + info.arrayNomsMetodes());
-
-
-
-//• modificador d'un atribut.
-//• modificador d'un mètode.
-//• modificar el valor d'un atribut.
-//• executar un dels seus mètodes.
-//• crear una instància.
-//• crear objecte clonat.
+        System.out.println("8. modificador d'un atribut: " + info.modificadorAtribut());
+        System.out.println("9. modificador d'un mètode: " + info.modificadorMetode());
+//        System.out.println("10.executar un dels seus mètodes: " + info.executarMetodes());
         
+        info.crearInstancia();
         
-//        Element el = new Element(4, "BATMAN");
-//        
-//        Method[] metodes = el.getClass().getDeclaredMethods();
-//        Method[] metodesTots = el.getClass().getMethods();
-//        
-//        
-//        System.out.println("TOTS els metodes: \n"
-//                + Arrays.toString(metodesTots));
-//        
-//        System.out.println("Metodes DECLARATS: \n"
-//                + Arrays.toString(metodes));
+        Element clonat = info.clone();
         
+        System.out.println("\n Clons");
+        System.out.println(e.toString());
+        System.out.println(info.toString());
+       
+
         
         
     }
